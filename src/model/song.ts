f3840e474ff5d7ddd96e02a3d08ba0686ff8e16e
Lyra Von Rose <lyravonrose@gmail.com>
2022-07-22 00:23:00 +0200

    playing songs

--- src/model/song.ts
+++ src/model/song.ts
@@ -1,5 +1,10 @@
+import { createNoSubstitutionTemplateLiteral } from "typescript";
+
 export const fetchSongs = () => {
     return fetch("https://api-stg.jam-community.com/song/trending")
         .then((response) => response.json())
-        .then((data) => data);
+        .then((data) => {
+            console.log("🤫", data);
+            return data;
+        });
 };
--- src/view/Songs.tsx
+++ src/view/Songs.tsx
@@ -1,13 +1,27 @@
 import React, { FC, useEffect, useState } from "react";
 import { fetchSongs } from "../model/song";
 import styles from "./Songs.module.css";
+import { Howl, Howler } from "howler";
 
 export const Songs: FC = () => {
     const [songs, setSongs] = useState([]);
+    const [chosenSong, setChosenSong] = useState<null | string>(null);
 
     useEffect(() => {
         fetchSongs().then((songs) => setSongs(songs));
     }, []);
+
+    const onPlay = (id: string) => {
+        setChosenSong(id);
+        const song: any = songs.find((song: any) => song.id === id);
+        if (song) {
+            const sound = new Howl({
+                src: [song.music_file_path],
+            });
+
+            sound.play();
+        }
+    };
 
     return (
         <div className={styles.container}>
@@ -15,7 +29,14 @@ export const Songs: FC = () => {
                 <div className={styles.song}>
                     <div className={styles.info}>name: {song.name}</div>
                     <div className={styles.actions}>
-                        <button>🌰</button>
+                        <button
+                            onClick={() => {
+                                onPlay(song.id);
+                            }}
+                        >
+                            {" "}
+                            PLAY
+                        </button>
                         <button>🥟</button>
                     </div>
                 </div>
